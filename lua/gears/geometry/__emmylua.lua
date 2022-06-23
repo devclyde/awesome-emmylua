4c1228256dbc422d8ce65fa3e21a99051a605ea0
devclyde <israel.devclyde@protonmail.com>
2022-06-23 19:49:48 -0300

    feat: add documentation for `gears.geometry`

---
+++ lua/gears/geometry/__emmylua.lua
@@ -0,0 +1,23 @@
+--- ### Description
+--- This file contains the EmmyLua based annotations for `gears.geometry`, you should not use `require` in this file.
+--- @module "gears.geometry.__emmylua"
+
+--#region Geometric Objects
+
+--- @class GoRectangle
+--- @field x number The horizontal coordinate.
+--- @field y number The verticate coordinate.
+--- @field width number The rectangle width.
+--- @field height number The rectangle height.
+
+--#endregion
+
+--#region Geometry Aliases
+
+--- @alias GeometryDirection
+--- | "up"
+--- | "down"
+--- | "right"
+--- | "left"
+
+--#endregion
